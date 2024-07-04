66e1c757ef00454fb92bdf74f5f3ac83e79aaed1
raymondsubianto <subiantoraymond20@gmail.com>
2024-07-05 00:43:01 +0700

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5000"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5000", "http://13.213.13.138:5000"];
 
 export default allowedOrigins;
